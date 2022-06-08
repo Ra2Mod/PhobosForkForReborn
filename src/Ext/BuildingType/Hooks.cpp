383867b21eb1fe8f12c060553b934b11f657aa57
Belonit <54427022+Belonit@users.noreply.github.com>
2022-06-08 06:06:56 +0300

    Configured PlacementPreview

--- src/Ext/BuildingType/Hooks.cpp
+++ src/Ext/BuildingType/Hooks.cpp
@@ -68,76 +68,71 @@ DEFINE_HOOK(0x458623, BuildingClass_KillOccupiers_Replace_MuzzleFix, 0x7)
 	return 0;
 }
 
-DEFINE_HOOK(0x6D528A, TacticalClass_DrawPlacement_PlacementPreview, 0x6)
+void TacticalClass_DrawPlacement_PlacementPreview()
 {
+	if(!Phobos::Config::EnableBuildingPlacementPreview)
+		return;
+
 	if (auto const pBuilding = specific_cast<BuildingClass*>(DisplayClass::Instance->CurrentBuilding))
 	{
 		if (auto const pType = pBuilding->Type)
 		{
-			auto const pTypeExt = BuildingTypeExt::ExtMap.Find(pType);
+			auto const pCell = MapClass::Instance->TryGetCellAt(Make_Global<CellStruct>(0x88095C) + Make_Global<CellStruct>(0x880960));
+			if (!pCell)
+				return;
 
-			if (pTypeExt && pTypeExt->PlacementPreview_Show.Get(Phobos::Config::EnableBuildingPlacementPreview))
+			SHPStruct* pImage = nullptr;
+			int nFrame = 0;
+
+			if (pImage = pType->LoadBuildup())
 			{
-				SHPStruct* Selected = nullptr;
-				bool bBuildupExist = false;
-
-				if (pType->LoadBuildup())
-				{
-					bBuildupExist = true;
-					Selected = pType->LoadBuildup();
-				}
-				else
-				{
-					Selected = pType->GetImage();
-				}
-
-				//bool const isUpgrade = GeneralUtils::IsValidString(pType->PowersUpBuilding);
-				auto const pImage = pTypeExt->PlacementPreview_Shape.Get(Selected);
-
-				if (!pImage)
-					return 0x0;
-
-				CellStruct const nDisplayCell = Make_Global<CellStruct>(0x88095C);
-				CellStruct const nDisplayCell_Offset = Make_Global<CellStruct>(0x880960);
-				auto const pCell = MapClass::Instance->TryGetCellAt(nDisplayCell + nDisplayCell_Offset);
-
-				if (!pCell)
-					return 0x0;
-
-				auto const nFrame = Math::clamp(pTypeExt->PlacementPreview_ShapeFrame.Get(bBuildupExist ? ((pImage->Frames / 2) - 1) : 0), 0, (int)pImage->Frames);
-				auto const nHeight = pCell->GetFloorHeight({ 0, 0 });
-				auto const nOffset = pTypeExt->PlacementPreview_Offset.Get();
-				Point2D nPoint { 0, 0 };
-				TacticalClass::Instance->CoordsToClient(CellClass::Cell2Coord(pCell->MapCoords, nHeight + nOffset.Z), &nPoint);
-				nPoint.X += nOffset.X;
-				nPoint.Y += nOffset.Y;
-				auto const nFlag = BlitterFlags::Centered | BlitterFlags::Nonzero | BlitterFlags::MultiPass | EnumFunctions::GetTranslucentLevel(pTypeExt->PlacementPreview_TranslucentLevel.Get(RulesExt::Global()->BuildingPlacementPreview_TranslucentLevel.Get()));
-				auto nRect = DSurface::Temp()->GetRect();
-				nRect.Height -= 32; // account for bottom bar
-				auto const pPalette = pTypeExt->PlacementPreview_Remap.Get() ? pBuilding->GetDrawer() : pTypeExt->PlacementPreview_Palette.GetOrDefaultConvert(FileSystem::UNITx_PAL());
-
-				DSurface::Temp()->DrawSHP(pPalette, pImage, nFrame, &nPoint, &nRect, nFlag,
-					0, 0, ZGradient::Ground, 1000, 0, nullptr, 0, 0, 0);
+				nFrame = (pImage->Frames / 2) - 1;
 			}
+			else
+			{
+				pImage = pType->GetImage();
+			}
+
+			auto const nHeight = pCell->GetFloorHeight({ 0, 0 });
+			CoordStruct const nOffset = { 0,-15,1 };
+			Point2D nPoint { 0, 0 };
+			TacticalClass::Instance->CoordsToClient(CellClass::Cell2Coord(pCell->MapCoords, nHeight + nOffset.Z), &nPoint);
+			nPoint.X += nOffset.X;
+			nPoint.Y += nOffset.Y;
+			auto nRect = DSurface::Temp()->GetRect();
+			nRect.Height -= 32; // account for bottom bar
+
+			auto const pPalette = pBuilding->GetDrawer();
+			auto const nFlag = BlitterFlags::Centered | BlitterFlags::Nonzero | BlitterFlags::MultiPass | BlitterFlags::TransLucent75;
+			DSurface::Temp()->DrawSHP(pPalette, pImage, nFrame, &nPoint, &nRect, nFlag, 0, 0, ZGradient::Ground, 1000, 0, nullptr, 0, 0, 0);
 		}
 	}
 
-	return 0x0;
+	return;
 }
-
-//Make Building placement Grid tranparent
 static void __fastcall CellClass_Draw_It_Shape(Surface* Surface, ConvertClass* Palette, SHPStruct* SHP, int FrameIndex,
 	const Point2D* const Position, const RectangleStruct* const Bounds, BlitterFlags Flags,
 	int Remap,
-	int ZAdjust, // + 1 = sqrt(3.0) pixels away from screen
+	int ZAdjust,
 	ZGradient ZGradientDescIndex,
-	int Brightness, // 0~2000. Final color = saturate(OriginalColor * Brightness / 1000.0f)
+	int Brightness,
 	int TintColor, SHPStruct* ZShape, int ZShapeFrame, int XOffset, int YOffset)
 {
-	Flags = Flags | EnumFunctions::GetTranslucentLevel(RulesExt::Global()->PlacementGrid_TranslucentLevel.Get());
+	if(Phobos::Config::EnableBuildingPlacementPreview)
+		Flags = Flags | BlitterFlags::TransLucent50;
 
 	CC_Draw_Shape(Surface, Palette, SHP, FrameIndex, Position, Bounds, Flags, Remap, ZAdjust,
 		ZGradientDescIndex, Brightness, TintColor, ZShape, ZShapeFrame, XOffset, YOffset);
 }
 
-DEFINE_POINTER_CALL(0x47EFB4, &CellClass_Draw_It_Shape);
+
+DEFINE_NAKED_LJMP(0x6D528A, _TacticalClass_DrawPlacement_PlacementPreview)
+{
+	_asm {call TacticalClass_DrawPlacement_PlacementPreview};
+	_asm {mov ecx, 0x880990};
+	_asm {mov ecx, [ecx]};
+	_asm {mov edi, 0x6D5290};
+	_asm {jmp edi};
+}
+
+DEFINE_POINTER_CALL(0x47EFB4, CellClass_Draw_It_Shape);
