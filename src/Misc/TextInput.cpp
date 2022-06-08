e3145084fb0e8f7da0ecaec23be3da25842680f3
Belonit <54427022+Belonit@users.noreply.github.com>
2022-06-08 05:58:13 +0300

    Update TextInput.cpp

--- src/Misc/TextInput.cpp
+++ src/Misc/TextInput.cpp
@@ -5,6 +5,7 @@
 
 wchar_t* IMEBuffer = reinterpret_cast<wchar_t*>(0xB730EC);
 
+inline
 UINT GetCurentCodepage()
 {
 	char szLCData[6 + 1];
@@ -15,29 +16,60 @@ UINT GetCurentCodepage()
 	return atoi(szLCData);
 }
 
-wchar_t LocalizeCaracter(char character)
-{
-	wchar_t result;
-	UINT codepage = GetCurentCodepage();
-	MultiByteToWideChar(codepage, MB_USEGLYPHCHARS, &character, 1, &result, 1);
-	return result;
-}
-
-DEFINE_HOOK(0x5D46C7, MessageListClass_Input, 5)
+DWORD _stdcall LocalizeCaracter(DWORD character)
 {
 	if (!IMEBuffer[0])
-		R->EBX<wchar_t>(LocalizeCaracter(R->EBX<char>()));
-
-	return 0;
+	{
+		wchar_t result;
+		UINT codepage = GetCurentCodepage();
+		char c = (char)character;
+		MultiByteToWideChar(codepage, MB_USEGLYPHCHARS, &c, 1, &result, 1);
+		return result;
+	}
+	return character;
 }
 
-DEFINE_HOOK(0x61526C, WWUI_NewEditCtrl, 5)
+DEFINE_NAKED_LJMP(0x5D46B9, MessageListClass_Input)
 {
-	if (!IMEBuffer[0])
-		R->EDI<wchar_t>(LocalizeCaracter(R->EDI<char>()));
+	_asm {push ebx};
+	_asm {call LocalizeCaracter};
+	_asm {mov ebx, eax};
 
-	return 0;
+	_asm {mov eax, [esi+2B0h]};
+
+	_asm {mov ecx, 0x5D46BF};
+	_asm {jmp ecx};
 }
+
+DEFINE_NAKED_LJMP(0x61526C, WWUI_NewEditCtrl)
+{
+	_asm {push edi};
+	_asm {call LocalizeCaracter};
+	_asm {mov edi, eax};
+
+	_asm {mov eax, [esi+40h]};
+	_asm {push eax};
+	_asm {push edi};
+
+	_asm {mov ecx, 0x615271};
+	_asm {jmp ecx};
+}
+
+//DEFINE_HOOK(0x5D46C7, MessageListClass_Input, 5)
+//{
+//	if (!IMEBuffer[0])
+//		R->EBX<wchar_t>(LocalizeCaracter(R->EBX<char>()));
+//
+//	return 0;
+//}
+//
+//DEFINE_HOOK(0x61526C, WWUI_NewEditCtrl, 5)
+//{
+//	if (!IMEBuffer[0])
+//		R->EDI<wchar_t>(LocalizeCaracter(R->EDI<char>()));
+//
+//	return 0;
+//}
 
 // It is required to add Imm32.lib to AdditionalDependencies
 /*
